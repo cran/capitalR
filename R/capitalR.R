0332a2c7b49acd28349c0b27d99b505008c9fb47
John T. Buynak <jbuynak94@gmail.com>
2019-05-09 18:00:02 +0000

    version 1.2.0

--- R/capitalR.R
+++ R/capitalR.R
@@ -63,7 +63,7 @@ geometric <- function(c) {
 r.calc <- function(vector) {
   r1 <- c(diff(vector)/vector)
   r1[is.na(r1)] <- 0
-  print(r1)
+  r1
 }
 
 
@@ -260,36 +260,100 @@ schedule <- function(r, n, pv, fv = 0, end = TRUE) {
 #' @param dates Vector of dates, the first date is the date of origination
 #' @param apr Annual rate
 #' @param pv Present Value
+#' @param info Logical, if set to 'TRUE' information about the dataframe arrangement will be printed
 #'
 #' @return Returns the irregular Amortization Schedule in a Dataframe
 #' @export
 #'
-#' @examples irregular(payments = c(0, 200, -100, 500),
-#' dates = c("2019-01-01", "2019-02-08", "2019-03-20", "2019-05-05"), apr = 0.05, pv = 2000)
-irregular <- function(payments, dates, apr, pv){
+#' @examples irregular(payments = c(0, 200, -100), dates = c("2019-01-01", "2019-02-08", "2019-03-20"),
+#' apr = 0.05, pv = 2000, info = FALSE)
+#'
+irregular <- function(payments, dates, apr, pv, info = TRUE){
+
+  Info <- data.frame(Header = rep(0,10), Details = rep(0,10))
+  Info$Header[1] <- "Dates"
+  Info$Header[2] <- "Payments"
+  Info$Header[3] <- "Cum.Pmts"
+  Info$Header[4] <- "Interest"
+  Info$Header[5] <- "Cum.Int"
+  Info$Header[6] <- "Accrued.Int"
+  Info$Header[7] <- "Int.Due"
+  Info$Header[8] <- "Principal"
+  Info$Header[9] <- "Cum.Prin"
+  Info$Header[10] <- "Balance"
+  Info$Details[1] <- "origination date and payment dates"
+  Info$Details[2] <- "individual payments"
+  Info$Details[3] <- "cumulative payments, running total of payments"
+  Info$Details[4] <- "interest portion of payment for the period"
+  Info$Details[5] <- "cumulative interest, running total of interest payments"
+  Info$Details[6] <- "interest which has accrued between this payment period and the last period"
+  Info$Details[7] <- "interest due, interest which has accrued and has yet to be paid"
+  Info$Details[8] <- "principal portion of the payments, reduces the balance of the loan"
+  Info$Details[9] <- "cumulative principal, running total of the principal payments"
+  Info$Details[10] <- "remaining balance of the loan"
+
+
+
+
 
   if(!payments[1]==0){
-    stop("Initial payment must be zero")
+    stop("The initial payment must be zero. If a payment was made on the origination date,
+         insert that date again in the 'dates' vector along with the payment in the 'payments' vector")
   }
+
+  if(length(payments)!=length(dates)){
+    stop("There is a difference of length between the 'payments' vector and the 'dates' vector.
+         Remember the first date must be the date of origination and the first payment must
+         be 0.")
+  }
+
   dates <- as.Date(dates, origin = "1970-01-01")
   Schedule <- data.frame(Dates = dates, Payments = 0, Cum.Pmts = 0, Interest = 0, Cum.Int = 0,
-                         Principal = 0, Cum.Prin = 0, Balance = pv)
+                         Accrued.Int = 0, Int.Due = 0, Principal = 0, Cum.Prin = 0, Balance = pv)
 
   for(i in 2:length(payments)){
     Schedule$Dates[i] <- dates[i]
     Schedule$Payments[i] <- payments[i]
     Schedule$Cum.Pmts <- cumsum(Schedule$Payments)
-    Schedule$Interest[i] <- ((as.numeric(dates[i])-as.numeric(dates[i-1]))/365 *apr*Schedule$Balance[i-1])
+
+    if(payments[i]< ((as.numeric(dates[i])-as.numeric(dates[i-1]))/365
+                     *apr*Schedule$Balance[i-1]) + Schedule$Int.Due[i-1]){
+      Schedule$Interest[i] <- payments[i]
+    }else{
+      Schedule$Interest[i] <- ((as.numeric(dates[i])-as.numeric(dates[i-1]))/365 *apr*Schedule$Balance[i-1]
+                               + Schedule$Int.Due[i-1])
+    }
     Schedule$Cum.Int <- cumsum(Schedule$Interest)
+    Schedule$Accrued.Int[i] <- ((as.numeric(dates[i])-as.numeric(dates[i-1]))/365
+                                *apr*Schedule$Balance[i-1]) #- Schedule$Cum.Int[i]
+    Schedule$Int.Due[i] <-  round(Schedule$Accrued.Int[i] - Schedule$Interest[i] + Schedule$Int.Due[i-1],2)
     Schedule$Principal[i] <- Schedule$Payments[i]-Schedule$Interest[i]
     Schedule$Cum.Prin <- cumsum(Schedule$Principal)
-    Schedule$Balance[i] <- pv - Schedule$Cum.Prin[i]
+
+
+    if(payments[i]< (((as.numeric(dates[i])-as.numeric(dates[i-1]))/365
+                      *apr*Schedule$Balance[i-1]) + Schedule$Int.Due[i-1])){
+
+      Schedule$Balance[i] <- Schedule$Balance[i-1] - Schedule$Principal[i] +
+        (Schedule$Accrued.Int[i] - payments[i])
+
+    }else{
+      Schedule$Balance[i] <- Schedule$Balance[i-1] - Schedule$Principal[i]
+    }
+
+
   }
 
 
-Schedule
+  Irregular <- list(Info, Schedule)
+  if(info == TRUE){
+    print(Irregular)
+  }else{
 
-}
+    print(Schedule)
+  }
+
+  }
 
 
 
